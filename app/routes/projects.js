db5af033af3a4880f193b136602ebc1e7ccc2f5c
GregMiras <gregmoiras@gmail.com>
2014-09-24 19:25:31 +0000

    added done button on tasks

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -5,45 +5,6 @@ var router = express.Router();
 
 /* GET Projects page. */
 router.get('/', function(req, res) {
-
-
-  // mongoose.model('tasks').find(function(err, tasks) {
-  //   tasks.populate(tasks, {
-  //     path: 'project'
-  //   }, function(err, tasks) {
-  //     tasks.where('name').equals('awd').find(function(err, tasks) {
-  //       console.log('tasks');
-  //     })
-  //   })
-  // });
-  //
-  // mongoose.model('tasks').find(function(err, tasks) {
-  //   mongoose.model('tasks').populate(tasks, {
-  //     path: 'project'
-  //   }, function(err, tasks) {
-  //
-  //       tasks.where('project._id').equals('542384bfdf72f6000042b0ea').find(function(err, tasks) {
-  //       console.log(tasks);
-  //     })
-  //   })
-  // });
-  //
-
-  // var project_id = req.param('project_id')
-  // project_id = "54238e93df72f6000042b0f4";
-  //
-  // mongoose.model('tasks').find(function(err, tasks) {
-  //   mongoose.model('tasks').populate(tasks, {
-  //     path: 'project'
-  //   }, function(err, tasks) {
-  //     mongoose.model('tasks').where('project._id').equals(project_id).remove(function(err, tasks){
-  //     });
-  //   });
-  // });
-
-
-
-
   mongoose.model('projects').find(function(err, projects) {
     res.send(projects)
   });
