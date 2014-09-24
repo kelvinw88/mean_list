96f3eceb5c08d821a578ad36765f5413c8a891d4
GregMiras <gregmoiras@gmail.com>
2014-09-24 17:53:59 +0000

    cleaned up tasks.html

--- public/js/services/projectData.js
+++ public/js/services/projectData.js
@@ -10,7 +10,6 @@ mean_list.factory('ProjectFactory', ['$http',function($http) {
       return $http.delete('/api/projects/' + id);
     },
     edit : function(Project) {
-      console.log("got into the routing");
       return $http.post('/api/projects/' + Project._id, Project);
     }
   }
