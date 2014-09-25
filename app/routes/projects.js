3a0ab6353fbb5eecf747683c5b484e6a991ab4b4
kelvinw88 <kelvinw88@gmail.com>
2014-09-25 10:53:25 -0700

    cascade delete for projects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -5,6 +5,45 @@ var router = express.Router();
 
 /* GET Projects page. */
 router.get('/', function(req, res) {
+
+
+  // mongoose.model('tasks').find(function(err, tasks) {
+  //   tasks.populate(tasks, {
+  //     path: 'project'
+  //   }, function(err, tasks) {
+  //     tasks.where('name').equals('awd').find(function(err, tasks) {
+  //       console.log('tasks');
+  //     })
+  //   })
+  // });
+  //
+  // mongoose.model('tasks').find(function(err, tasks) {
+  //   mongoose.model('tasks').populate(tasks, {
+  //     path: 'project'
+  //   }, function(err, tasks) {
+  //
+  //       tasks.where('project._id').equals('542384bfdf72f6000042b0ea').find(function(err, tasks) {
+  //       console.log(tasks);
+  //     })
+  //   })
+  // });
+  //
+
+  // var project_id = req.param('project_id')
+  // project_id = "54238e93df72f6000042b0f4";
+  //
+  // mongoose.model('tasks').find(function(err, tasks) {
+  //   mongoose.model('tasks').populate(tasks, {
+  //     path: 'project'
+  //   }, function(err, tasks) {
+  //     mongoose.model('tasks').where('project._id').equals(project_id).remove(function(err, tasks){
+  //     });
+  //   });
+  // });
+
+
+
+
   mongoose.model('projects').find(function(err, projects) {
     res.send(projects)
   });
@@ -56,15 +95,30 @@ router.post('/:project_id', function(req, res) {
 /* delete a project. */
 router.delete('/:project_id', function(req, res) {
   var project = mongoose.model('projects');
-  project.remove({
-    _id: req.params.project_id
-  }, function(err, project) {
+  var tasks = mongoose.model('tasks');
+
+
+
+
+project.remove({
+  _id: req.params.project_id
+}, function(err, project) {
+  if (err)
+    res.send(err);
+  else
+  tasks.remove({
+    project: req.params.project_id
+  }, function(err, tasks) {
     if (err)
       res.send(err);
-        mongoose.model('projects').find(function(err, projects) {
-      res.send(projects)
-    });
+    // mongoose.model('tasks').find(function(err, tasks) {});
+    // });
+  // send back the new projects file.
+  mongoose.model('projects').find(function(err, projects) {
+    res.send(projects)
   });
+});
+
 });
 
 module.exports = router;
