c12aba9238518ff2109f5eb3ee406cf6e0a394cb
Greg Moiras <gregmoiras@gmail.com>
2014-09-25 21:23:03 +0000

    added datepicker in pop up window

--- public/js/controllers/DatepickerDemo.js
+++ public/js/controllers/DatepickerDemo.js
@@ -1,37 +1,30 @@
-
-var ModalDemoCtrl = function ($scope, $modal, $log) {
-
-  $scope.open = function () {
-
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: ModalInstanceCtrl
-    });
-
-    modalInstance.result.then(function (selected) {
-      $scope.selected = selected;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
+mean_list.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
   };
-};
+  $scope.today();
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, $timeout) {
-
-$scope.dt = new Date();
-
-  $scope.open = function() {
-    $timeout(function() {
-      $scope.opened = true;
-    });
+  $scope.clear = function () {
+    $scope.dt = null;
   };
 
-  
-  $scope.ok = function () {
-    $modalInstance.close($scope.dt);
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
   };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
   };
-};
+
+  $scope.formats = ['dd-MMMM-yyyy'];
+  $scope.format = $scope.formats[0];
+});
--- public/js/controllers/popUpWindow.js
+++ public/js/controllers/popUpWindow.js
@@ -46,6 +46,36 @@ $scope.selectStatus = function() {
     $scope.tasks = data;
   });
   }
+//datepicker starts
+$scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy'];
+  $scope.format = $scope.formats[0];
+//datepicker ends
 
 });
 
