d997fd5f0182f05e32c941fc6932b0cec1262de0
Greg Moiras <gregmoiras@gmail.com>
2014-09-28 21:46:01 +0000

    added create user method for signup

--- app/models/project.js
+++ app/models/project.js
@@ -3,7 +3,13 @@ var Schema = mongoose.Schema;
 
 var projectsSchema = new Schema({
   name: {type: String, required: true},
-  time : { type : Date, default: Date.now }
+  time : { type : Date, default: Date.now },
+  users: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    default: [],
+    required: true
+  },
 });
 
 mongoose.model('projects', projectsSchema);
--- public/js/services/userData.js
+++ public/js/services/userData.js
@@ -1,19 +1,19 @@
 mean_list.factory('UserFactory', ['$http',function($http) {
   return {
     get : function() {
-      return $http.get('/api/projects');
+      return $http.get('/api/users');
     },
     get_a_project : function(id){
-      return $http.get('/api/projects/' + id);
+      return $http.get('/api/users/' + id);
     },
-    create : function(ProjectData) {
-      return $http.post('/api/projects', ProjectData);
+    create : function(UserData) {
+      return $http.post('/api/users', UserData);
     },
     delete : function(id) {
-      return $http.delete('/api/projects/' + id);
+      return $http.delete('/api/users/' + id);
     },
     edit : function(Project) {
-      return $http.post('/api/projects/' + Project._id, Project);
+      return $http.post('/api/users/' + User._id, User);
     }
   }
 }]);
