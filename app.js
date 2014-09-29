9b86cb1105b9629477e3d7f050302abfd5474c92
Greg Moiras <gregmoiras@gmail.com>
2014-09-29 18:15:01 +0000

    added auth.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var authController = require('./app/routes/auth');
 
 //SET UP DATABASE
 var mongo = require('mongodb');
