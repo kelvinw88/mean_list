312754da873609ac5f4d2245618a74cb7b0bee52
kelvinw88 <kelvinw88@gmail.com>
2014-09-29 19:46:14 -0700

    added user model

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,127 +1,34 @@
 var mongoose = require('mongoose');
 var express = require('express');
+var crypto = require('crypto');
 var router = express.Router();
-var util = require('util');
-
-function inspect(o, depth)
-{
-  return console.log(util.inspect(o, { colors: true, depth: depth || 1 }));
-}
 
 
-/* GET Tasks page. */
-router.get('/', function(req, res) {
-  //get all tasks with time sort
-  mongoose.model('tasks').find().sort([
-    ['time', 'descending']
-  ]).find(function(err, tasks) {
-    mongoose.model('tasks').populate(tasks, {
-      path: 'project'
-    }, function(err, tasks) {
-      res.send(tasks);
-    })
-  });
+/* GET Projects page. */
+router.post('/login', function(req, res) {
+
+  var users = mongoose.model('users');
+  var username = req.body.username;
+  var password = req.body.password;
+
+  users.where('username').equals(username).find(function(err, users) {
+    res.send(users);
+  })
 });
 
-
-router.get('/project/:project_id', function(req, res) {
-
-
-  var project_id = req.param('project_id')
-  mongoose.model('tasks').find().sort([
-    ['time', 'descending']
-  ]).find(function(err, tasks) {
-    mongoose.model('tasks').populate(tasks, {
-      path: 'project'
-    }, function(err, tasks) {
-      mongoose.model('tasks').where('project').equals(project_id).find(function(err, tasks) {
-        res.send(tasks);
-      })
-    })
-  });
-});
-
-
-/*
-curl -X POST -H "Content-Type: application/json" -d '{"name":"totleraf"}' http://localhost:3000/api/tasks
-*/
-/* post Task page. */
-router.post('/', function(req, res) {
+router.post('/create', function(req, res) {
   var users = mongoose.model('users');
   var user = new users();
 
-  user.name = req.body.name;
-  user.password = req.body.email;
+  user.username = req.body.username;
+  user.password = req.body.password;
   user.email = req.body.email;
-  
 
-  task.save(function(err) {
+  user.save(function(err){
     if (err)
       res.send(err);
-    res.send(user);
-  });
-});
-
-/* get a task. */
-router.get('/:task_id', function(req, res) {
-  var task = mongoose.model('tasks');
-  task.findById(req.params.task_id, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-});
-
-
-/* edit a task. */
-router.post('/:task_id', function(req, res)
-{
-  var task = mongoose.model('tasks');
-  task.findById(req.params.task_id, function(err, task)
-  {
-    if (err)
-    {
-      console.error(err);
-      return res.send(err);
-    }
-
-    task.name = req.body.name;
-    task.description = req.body.description;
-    task.done = req.body.done;
-    task.project = req.body.project;
-    task.status = req.body.status;
-    task.due_date = req.body.due_date;
-    task.progress_bar = req.body.progress_bar;
-    task.time_estimate = req.body.time_estimate;
-    task.sub_task = req.body.sub_task;
-    task.attachment = req.body.attachment;
-    task.save(function(err)
-    {
-      if (err)
-      {
-        console.log("Error: %s", err.message)
-        return res.send(err);
-      }
-      return res.json(task);
-    });
-  });
-});
-
-
-/* delete a task. */
-router.delete('/:task_id', function(req, res) {
-  var task = mongoose.model('tasks');
-  task.remove({
-    _id: req.params.task_id
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    mongoose.model('tasks').populate(tasks, {
-      path: 'project'
-    }, function(err, tasks) {
-      res.send(tasks);
-    })
-  });
+    res.send(user._id);
+  })
 });
 
 
