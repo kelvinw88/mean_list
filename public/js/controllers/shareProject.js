a20711f2d8d76668471b26b028a413fd9bf0bd79
kelvinw88 <kelvinw88@gmail.com>
2014-10-02 02:27:35 -0700

    serach for users bar

--- public/js/controllers/shareProject.js
+++ public/js/controllers/shareProject.js
@@ -1,6 +1,16 @@
-mean_list.controller('ShareProjectCtrl', function($scope, $filter, $http, UserFactory, ProjectFactory, $rootScope, $location, notify) {
+mean_list.controller('ShareProjectCtrl', function($scope,$stateParams, $filter, $http, UserFactory, ProjectFactory, $rootScope, $location, notify) {
 
   console.log("Share Project");
+  console.log($stateParams.project_id);
+
+  $scope.project_name = "";
+
+  ProjectFactory.get_a_project($stateParams.project_id)
+  .success(function(data) {
+
+    $scope.project_name = data.name;
+  });
+
   UserFactory.get_all_username()
     .success(function(data) {
       console.log(data);
